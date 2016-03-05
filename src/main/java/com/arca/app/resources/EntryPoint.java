dbd327224a6b1935da3538ad10f3dd05c1309693
Nicolas Machut <machut.nicolas@gmail.com>
2016-03-05 14:14:52 +0100

    premiere ressource rest

--- src/main/java/com/arca/app/resources/EntryPoint.java
+++ src/main/java/com/arca/app/resources/EntryPoint.java
@@ -2,9 +2,7 @@ package com.arca.app.resources;
 
 import com.arca.app.dao.LineDao;
 import com.arca.app.domain.Line;
-import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.ws.rs.GET;
@@ -12,8 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 /**
@@ -32,16 +28,20 @@ public class EntryPoint {
     @GET
     @Path("test")
     @Produces(MediaType.APPLICATION_JSON)
-    public String test () {
+    public Response test () {
         System.out.println("hrllo");
-        //List<Line> lines = lineDao.getAll();
-            //String json = new ObjectMapper().writeValueAsString(lines);
-        /*return Response.
+        List<Line> lines = lineDao.getAll();
+        String json = null;
+        try {
+            json = new ObjectMapper().writeValueAsString(lines);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return Response.
                 ok(json)
                 .header("Access-Control-Allow-Origin", "*")
                 .header("Access-Control-Allow-Methods", "*")
                 .allow("OPTIONS")
-                .build();*/
-        return "test";
+                .build();
     }
 }
